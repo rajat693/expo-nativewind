41840dae99673a2fea1833dd6f05d5d5a8e25b96
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-03-18 09:24:15 +0530

    fix: nativewind and themed provider together

--- App.tsx
+++ App.tsx
@@ -1,24 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  View } from 'react-native';
-import "./global.css"
-import { GluestackUIProvider } from './components/gluestack-ui-provider';
-import {Text} from "./components/ui/text"
-export default function App() {
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet,  View } from 'react-native';
+// import "./global.css"
+// import { GluestackUIProvider } from './components/gluestack-ui-provider';
+// import {Text} from "./components/ui/text"
+// export default function App() {
+//   return (
+//     <GluestackUIProvider>
+//     <View style={styles.container}>
+//       <Text size='xl'>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//     </GluestackUIProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import "./global.css";
+import React from "react";
+import {
+  GluestackUIProvider as ThemedProvider,
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  Button,
+  ButtonGroup,
+  ButtonText,
+  Center,
+  CloseIcon,
+  Icon,
+  // Heading
+} from "@gluestack-ui/themed";
+import { Text } from "./components/ui/text";
+import { Heading } from "./components/ui/heading";
+import { GluestackUIProvider as NativewindProvider } from "./components/gluestack-ui-provider";
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
+
+function Example() {
+  const [showAlertDialog, setShowAlertDialog] = React.useState(false);
   return (
-    <GluestackUIProvider>
-    <View style={styles.container}>
-      <Text size='xl'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </GluestackUIProvider>
+    <Center flex={1}>
+      <Button onPress={() => setShowAlertDialog(true)}>
+        <ButtonText>Click me</ButtonText>
+      </Button>
+      <AlertDialog
+        isOpen={showAlertDialog}
+        onClose={() => {
+          setShowAlertDialog(false);
+        }}
+      >
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading size="lg" className="leading-3">
+              Deactivate account
+            </Heading>
+            <AlertDialogCloseButton>
+              <Icon as={CloseIcon} />
+            </AlertDialogCloseButton>
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            <Text size="sm">
+              Are you sure you want to deactivate your account? Your data will
+              be permanently removed and cannot be undone.
+            </Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <ButtonGroup space="lg">
+              <Button
+                variant="outline"
+                action="secondary"
+                onPress={() => {
+                  setShowAlertDialog(false);
+                }}
+              >
+                <ButtonText>Cancel</ButtonText>
+              </Button>
+              <Button
+                bg="$error600"
+                action="negative"
+                onPress={() => {
+                  setShowAlertDialog(false);
+                }}
+              >
+                <ButtonText>Deactivate</ButtonText>
+              </Button>
+            </ButtonGroup>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </Center>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NativewindProvider>
+      <ThemedProvider config={config}>
+        <Example />
+      </ThemedProvider>
+    </NativewindProvider>
+  );
+}
